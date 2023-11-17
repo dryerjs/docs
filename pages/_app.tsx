9a407d0f1f9011e7f6b2f565f8a4e5a14bda2478
Brian Nguyen <vanpho01@gmail.com>
2023-11-17 15:49:02 +0700

    feat: add structure (#24)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import "../styles/main.scss";
 
 export const runtime = "experimental-edge";
+import { NextSeo } from 'next-seo';
+
+const Page = () => (
+  <>
+    <NextSeo
+      title="Simple Usage Example"
+      description="A short description goes here."
+    />
+    <p>Simple Usage</p>
+  </>
+);
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps}><Page /></Component>;
 }
