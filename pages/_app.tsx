b0102393d5f3d3d97a06516e283bcefc3710e442
Brian Nguyen <vanpho02@gmail.com>
2023-11-18 12:24:05 +0700

    feat: transform andd validate
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,7 @@
 import "../styles/main.scss";
 
 export const runtime = "experimental-edge";
-import { NextSeo } from 'next-seo';
-
-const Page = () => (
-  <>
-    <NextSeo
-      title="Simple Usage Example"
-      description="A short description goes here."
-    />
-    <p>Simple Usage</p>
-  </>
-);
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}><Page /></Component>;
+  return <Component {...pageProps} />;
 }
