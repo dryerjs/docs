b0466d40270816b7b9bd99856c82973ed9f6cdef
Tim Lai <tuanvu0995@gmail.com>
2023-11-20 18:22:07 +0700

    feat: Add sitemap and gtag
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
+import Script from "next/script";
 import "../styles/main.scss";
 
 export const runtime = "experimental-edge";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-L709X1Q4RB" />
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-L709X1Q4RB');
+      `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
