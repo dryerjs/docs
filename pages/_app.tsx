952c9ac34da7b94d51d9862de9fce526ac0955cf
Brian Nguyen <vanpho02@gmail.com>
2024-02-04 12:47:40 +0700

    feat: hotjar
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,21 @@ export default function MyApp({ Component, pageProps }) {
         }, 60000);
       `}
       </Script>
+      <Script>
+        {`
+        <!-- Hotjar Tracking Code for https://dryerjs.com -->
+        <script>
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3852479,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
